95be4890fe529cb83608219cbe7fb577dcb48b66
SoraSpades <davidandrino2002@gmail.com>
2021-08-18 21:47:36 +0200

    About page

--- src/index.js
+++ src/index.js
@@ -11,11 +11,17 @@ app.get("/", async (req, res) => { // Main page
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
+app.get("/about", async (req, res) => { // About page
+    res.sendFile(path.join(__dirname, "../public/about.html"))
+})
+
 app.get("/query", async (req, res) => { // Query page
     if (!req.query.word) return res.status(400).send()
     const results = await makeRequest(req.query.word, req.query.max ? req.query.max : 5)
     res.send(results.map(r=>parseResult(r)))
 })
+
+
 
 // Express init
 app.listen(process.env.PORT, () => console.log("Listening on port ", process.env.PORT))
