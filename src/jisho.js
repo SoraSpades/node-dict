0f83e290b93277767cd4b0082ba450d5cde09245
SoraSpades <davidandrino2002@gmail.com>
2021-08-18 14:19:50 +0200

    Create parseResult function

--- src/jisho.js
+++ src/jisho.js
@@ -5,15 +5,41 @@ const axios = require("axios")
  * Make a request to the Jisho API
  * @param {String} word - Word to search
  * @param {Number} maxResults - Max results to show, default 5
- * @returns First results up to the number specified
+ * @returns {Array} First results up to the number specified
  */
 const makeRequest = async (word, maxResults=5) => {
     try {
         const response = await axios.get(encodeURI("https://jisho.org/api/v1/search/words?keyword=" + word))
-        console.log(response.data.data.slice(0, maxResults))
+        return response.data.data.slice(0, maxResults)
     } catch (e) {
         console.log(e)
     }
 }
 
-module.exports = { makeRequest }
+
+/**
+ * @typedef {Object} parsedResult
+ * @property {String} japanese - Japanese word (with kanji)
+ * @property {String} japaneseReading - Japanese reading (Hiragana)
+ * @property {String[]} definitions - English definitions
+ * @property {String} [jlpt] - Japanese level by the JLPT standard
+ */
+/**
+ * Transforms the result from an API in a more friendly, one-leveled object
+ * @param {Object} result - Result from an API call
+ * @returns {parsedResult} Transformed Object
+ */
+const parseResult = (result) => {
+    const parsedResult = {
+        japanese        : result.japanese[0].word,
+        japaneseReading : result.japanese[0].reading,
+        definitions     : [],
+    }
+    if (result.jlpt[0]) parsedResult.jlpt = result.jlpt[0]
+    result.senses.forEach(sense => {
+        parsedResult.definitions.push(sense.english_definitions.join(", "))
+    });
+    return parsedResult
+}
+
+module.exports = { makeRequest, parseResult }
