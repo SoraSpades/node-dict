7a6166062d35bc5063690ab1b5745f73152da401
SoraSpades <davidandrino2002@gmail.com>
2021-08-18 13:48:35 +0200

    Document makeRequest function

--- src/jisho.js
+++ src/jisho.js
@@ -1,6 +1,12 @@
 // File containing Jisho-API related functions
 const axios = require("axios")
 
+/**
+ * Make a request to the Jisho API
+ * @param {String} word - Word to search
+ * @param {Number} maxResults - Max results to show, default 5
+ * @returns First results up to the number specified
+ */
 const makeRequest = async (word, maxResults=5) => {
     try {
         const response = await axios.get(encodeURI("https://jisho.org/api/v1/search/words?keyword=" + word))
