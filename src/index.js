251b7827275cc89ae9a1647bef5f45dd1fb27084
SoraSpades <davidandrino2002@gmail.com>
2021-08-18 21:00:05 +0200

    Begin creation of main page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 const { makeRequest, parseResult } = require("./jisho")
 
 // Express config
@@ -6,7 +7,11 @@ const app = express()
 app.use(express.static("/public"))
 
 // Express endpoints
-app.get("/query", async (req, res) => {
+app.get("/", async (req, res) => { // Main page
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+app.get("/query", async (req, res) => { // Query page
     if (!req.query.word) return res.status(400).send()
     const results = await makeRequest(req.query.word, req.query.max ? req.query.max : 5)
     res.send(results.map(r=>parseResult(r)))
